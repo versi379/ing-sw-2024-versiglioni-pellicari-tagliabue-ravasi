c1b9e86943e68e8679d81d12c3602135405ae146
Pietro_Ravasi <pietro2.ravasi@mail.polimi.it>
2024-03-12 15:27:17 +0100

    Creation Project directory 2

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module it.polimi.sw.gianpaolocugola50 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens it.polimi.sw.gianpaolocugola50 to javafx.fxml;
+    exports it.polimi.sw.gianpaolocugola50;
+}
