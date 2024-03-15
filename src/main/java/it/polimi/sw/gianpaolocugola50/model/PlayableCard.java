779ddb731868750b418b5a43cae62155db93a0ff
Francesco-Tagliabue <francescogiuseppe.tagliabue@mail.polimi.it>
2024-03-15 10:46:34 +0100

    Aggiunta interfaccia Bonus Aggiunto metodo resourceCount Modifiche varie

--- src/main/java/it/polimi/sw/gianpaolocugola50/model/PlayableCard.java
+++ src/main/java/it/polimi/sw/gianpaolocugola50/model/PlayableCard.java
@@ -1,21 +1,43 @@
 package it.polimi.sw.gianpaolocugola50.model;
-import java.util.List
+import java.util.*;
+import static java.util.stream.Stream.concat;
 
 public class PlayableCard {
-    private Color color;
-    private int score;
-    private List<Resource> fixedResources;
-    private Corner swCorner;
-    private Corner nwCorner;
-    private Corner neCorner;
-    private Corner seCorner;
+    private final Color color;
+    private final int score;
+    private final List<Resource> fixedResources;
+    private final List<Corner> corners;
 
-    public getColor()
-    public getScore()
-    public getFixedResources()
-    public getSwCorner()
-    public getNwCorner()
-    public getNeCorner()
-    public getSeCorner()
+    public PlayableCard(Color color, int score, List<Resource> fixedResources, List<Corner> corners) {
+        this.color = color;
+        this.score = score;
+        this.fixedResources = new ArrayList<>(fixedResources);
+        this.corners = new ArrayList<>(corners);
+    }
+    public Color getColor() {
+        return color;
+    }
+    public int getScore() {
+        return score;
+    }
+    public List<Resource> getFixedResources() {
+        return new ArrayList<>(fixedResources);
+    }
+    public Corner getSwCorner() {
+        return corners.get(0);
+    }
+    public Corner getNwCorner() {
+        return corners.get(1);
+    }
+    public Corner getNeCorner() {
+        return corners.get(2);
+    }
+    public Corner getSeCorner() {
+        return corners.get(3);
+    }
+    public int resourceCount(Resource targetResource) {
+        return (int)concat(corners.stream().map(Corner::getResource), fixedResources.stream())
+                .filter(x -> x.equals(targetResource))
+                .count();
     }
 }
