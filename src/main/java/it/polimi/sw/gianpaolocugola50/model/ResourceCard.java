823c2d7b482b6b66c1d8a6cd7cc6ffdbc824d184
Francesco-Tagliabue <francescogiuseppe.tagliabue@mail.polimi.it>
2024-03-18 17:17:11 +0100

    Modifiche metodi

--- src/main/java/it/polimi/sw/gianpaolocugola50/model/ResourceCard.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.sw.gianpaolocugola50.model;
-
-public class ResourceCard extends RGCard{
-
-}
--- src/main/java/it/polimi/sw/gianpaolocugola50/model/StarterCard.java
+++
@@ -1,5 +0,0 @@
-package it.polimi.sw.gianpaolocugola50.model;
-
-public class StarterCard extends PlayableCard {
-    private List<Resource>
-}
