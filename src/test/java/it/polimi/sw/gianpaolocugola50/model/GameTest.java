053430413d0e7b5ec98be673c26c6eac5e5f4102
Pietro_Ravasi <pietro2.ravasi@mail.polimi.it>
2024-03-19 13:00:13 +0100

    creation of the test class of game

---
+++ src/test/java/it/polimi/sw/gianpaolocugola50/model/GameTest.java
@@ -0,0 +1,16 @@
+package it.polimi.sw.gianpaolocugola50.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void start() {
+    }
+}
