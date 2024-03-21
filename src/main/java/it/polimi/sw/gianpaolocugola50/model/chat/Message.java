961cf3b4021bffef53709dc7aaebe185e5beb9df
Giovanni <giovanniversiglioni@outlook.it>
2024-03-21 14:16:46 +0100

    Model: Chat classes structure (Message, Chat)

--- src/main/java/it/polimi/sw/gianpaolocugola50/model/chat/Message.java
+++ src/main/java/it/polimi/sw/gianpaolocugola50/model/chat/Message.java
@@ -1,26 +1,61 @@
 package it.polimi.sw.gianpaolocugola50.model.chat;
 
-import it.polimi.sw.gianpaolocugola50.model.game.Player;
-
-import java.io.Serializable;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-public class Message implements Serializable {
+import com.google.gson.annotations.Expose;
 
-    private String text;
-    private Player sender;
-    private LocalTime time;
+/**
+ * This class defines messages that players can send to each other
+ */
+public class Message {
 
-    //private int x;
+    @Expose
+    private final Player sender;
+    @Expose
+    private final String content;
+    @Expose
+    private final String time;
+    @Expose
+    private final Player receiver;
 
-    public Message(String text, Player sender) {
-        this.text = text;
+    /**
+     * This constructor creates a broadcast message (to all other players)
+     */
+    public Message(Player sender, String content, LocalTime time) {
         this.sender = sender;
-        this.time = java.time.LocalTime.now();
+        this.content = content;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+        this.time = time.format(formatter);;
+        this.receiver = null;
     }
 
-    public String toString(){
-        return null;
-    } // to do
+    /**
+     * This constructor creates a message between sender and receiver
+     */
+    public Message(Player sender, String content, LocalTime time, Player receiver) throws UnsupportedOperationException {
+        if(sender.equals(receiver)) throw new UnsupportedOperationException("You can't send a message to yourself!");
+        this.sender = sender;
+        this.content = content;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+        this.time = time.format(formatter);;
+        this.receiver = receiver;
+    }
+    
+    public Player getSender() {
+        return sender;
+    }
 
+    public String getContent() {
+        return content;
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+
+    public Player getReceiver() {
+        return receiver;
+    }
+    
 }
