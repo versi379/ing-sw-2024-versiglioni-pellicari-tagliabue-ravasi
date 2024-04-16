35e02f46926f1c648ae0be99a90d7b35adf1cebb
Giovanni <giovanniversiglioni@outlook.it>
2024-04-16 14:19:26 +0200

    test classes chat and card

---
+++ src/test/java/it/polimi/sw/gianpaolocugola50/model/card/BlankBonusTest.java
@@ -0,0 +1,12 @@
+package it.polimi.sw.gianpaolocugola50.model.card;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlankBonusTest {
+
+    @Test
+    void checkBonus() {
+    }
+}
---
+++ src/test/java/it/polimi/sw/gianpaolocugola50/model/card/CornerTest.java
@@ -0,0 +1,24 @@
+package it.polimi.sw.gianpaolocugola50.model.card;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CornerTest {
+
+    @Test
+    void isVisible() {
+    }
+
+    @Test
+    void isFull() {
+    }
+
+    @Test
+    void getResource() {
+    }
+
+    @Test
+    void getStatus() {
+    }
+}
