772ea85b7a1355228f6668f36a6cb471f23eef5c
Francesco-Tagliabue <francescogiuseppe.tagliabue@mail.polimi.it>
2024-04-16 14:20:50 +0200

    a

---
+++ src/test/java/it/polimi/sw/gianpaolocugola50/model/game/PlayerTest.java
@@ -0,0 +1,40 @@
+package it.polimi.sw.gianpaolocugola50.model.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getNickName() {
+    }
+
+    @Test
+    void setCurrentGame() {
+    }
+
+    @Test
+    void getCurrentGame() {
+    }
+
+    @Test
+    void setStatus() {
+    }
+
+    @Test
+    void getStatus() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/test/java/it/polimi/sw/gianpaolocugola50/model/objective/MonolithObjectiveTest.java
@@ -0,0 +1,20 @@
+package it.polimi.sw.gianpaolocugola50.model.objective;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonolithObjectiveTest {
+
+    @Test
+    void getTargetColor() {
+    }
+
+    @Test
+    void getOrientation() {
+    }
+
+    @Test
+    void checkCondition() {
+    }
+}
