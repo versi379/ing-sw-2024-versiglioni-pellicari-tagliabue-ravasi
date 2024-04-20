2a9d44cfe4ee179e78be5a990667d16573c2bdd1
Francesco-Tagliabue <francescogiuseppe.tagliabue@mail.polimi.it>
2024-04-20 10:47:35 +0200

    aggiunti test

--- src/test/java/it/polimi/sw/GC50/model/objective/CaveObjectiveTest.java
+++ src/test/java/it/polimi/sw/GC50/model/objective/CaveObjectiveTest.java
@@ -17,58 +17,16 @@ class CaveObjectiveTest {
         assertEquals(CaveOrientation.INVERTEDL, caveObjective.getOrientation());
     }
 
-    /**
-     * Check matrix operations to be performed on CardsMatrix,
-     * with respect to different cave orientation cases
-     */
     @Test
     void testAdjustOrientation() {
-        // Create a test CardsMatrix
-        Corner emptyCorner = new Corner(CornerStatus.EMPTY, null);
-        Corner[] corners = new Corner[]{emptyCorner, emptyCorner, emptyCorner, emptyCorner};
-        PlayableCard greenPlayableCard = new PlayableCard(Color.GREEN, 1, corners);
-        PlayableCard bluePlayableCard = new PlayableCard(Color.BLUE, 1, corners);
-        PlayableCard redPlayableCard = new PlayableCard(Color.RED, 1, corners);
-        PlayableCard[][] testPlayableCards = {
-                {greenPlayableCard, greenPlayableCard, bluePlayableCard, bluePlayableCard},
-                {redPlayableCard, redPlayableCard, greenPlayableCard, greenPlayableCard},
-                {greenPlayableCard, greenPlayableCard, bluePlayableCard, bluePlayableCard},
-                {redPlayableCard, redPlayableCard, greenPlayableCard, greenPlayableCard},
-        };
-        CardsMatrix testCardsMatrix = new CardsMatrix(testPlayableCards.length);
-        testCardsMatrix.setCardsMatrix(testPlayableCards);
 
-        // Test when orientation is INVERTEDL
-        CaveObjective caveObjectiveInvertedL = new CaveObjective(Color.BLUE, Color.RED, CaveOrientation.INVERTEDL);
-        CardsMatrix invertedLResult = caveObjectiveInvertedL.adjustOrientation(testCardsMatrix);
-        // Assert that the result is transposed
-        assertEquals(testCardsMatrix.transpose(), invertedLResult);
-
-        // Test when orientation is INVERTEDJ
-        CaveObjective caveObjectiveInvertedJ = new CaveObjective(Color.BLUE, Color.RED, CaveOrientation.INVERTEDJ);
-        CardsMatrix invertedJResult = caveObjectiveInvertedJ.adjustOrientation(testCardsMatrix);
-        // Assert that the result is inverted
-        assertEquals(testCardsMatrix.invert(), invertedJResult);
-
-        // Test when orientation is UPRIGHTJ
-        CaveObjective caveObjectiveUprightJ = new CaveObjective(Color.BLUE, Color.RED, CaveOrientation.UPRIGHTJ);
-        CardsMatrix uprightJResult = caveObjectiveUprightJ.adjustOrientation(testCardsMatrix);
-        // Assert that the result is transposed and then inverted
-        assertEquals(testCardsMatrix.transpose().invert(), uprightJResult);
-
-        // Test when orientation is UPRIGHTL
-        CaveObjective caveObjectiveUprightL = new CaveObjective(Color.BLUE, Color.RED, CaveOrientation.UPRIGHTL);
-        CardsMatrix defaultResult = caveObjectiveUprightL.adjustOrientation(testCardsMatrix);
-        // Assert that the result is the same as the input matrix
-        assertEquals(testCardsMatrix, defaultResult);
     }
 
     /**
-     * Test InvertedL cave condition, where the two bottom cards are blue,
-     * and the top card is red
+     *
      */
     @Test
-    void testInvertedLCondition() {
+    void testUprightLCondition() {
         Corner emptyCorner = new Corner(CornerStatus.EMPTY, null);
         Corner[] corners = new Corner[]{emptyCorner, emptyCorner, emptyCorner, emptyCorner};
         PlayableCard genericPlayableCard = new PlayableCard(Color.GREEN, 1, corners);
@@ -76,16 +34,31 @@ class CaveObjectiveTest {
         PlayableCard redPlayableCard = new PlayableCard(Color.RED, 1, corners);
         PlayableCard[][] testPlayableCards = {
                 {genericPlayableCard, genericPlayableCard, genericPlayableCard, genericPlayableCard, genericPlayableCard},
-                {genericPlayableCard, genericPlayableCard, bluePlayableCard, redPlayableCard, genericPlayableCard},
-                {genericPlayableCard, bluePlayableCard, genericPlayableCard, genericPlayableCard, genericPlayableCard},
+                {genericPlayableCard, redPlayableCard, bluePlayableCard, genericPlayableCard, genericPlayableCard},
+                {genericPlayableCard, genericPlayableCard, genericPlayableCard, bluePlayableCard, genericPlayableCard},
                 {genericPlayableCard, genericPlayableCard, genericPlayableCard, genericPlayableCard, genericPlayableCard},
                 {genericPlayableCard, genericPlayableCard, genericPlayableCard, genericPlayableCard, genericPlayableCard}
         };
         CardsMatrix testCardsMatrix = new CardsMatrix(testPlayableCards.length);
         testCardsMatrix.setCardsMatrix(testPlayableCards);
         PlayerData testPlayerData = new PlayerData(testCardsMatrix);
-        CaveObjective caveObjective = new CaveObjective(Color.BLUE, Color.RED, CaveOrientation.INVERTEDL);
-        assertEquals(3, caveObjective.checkCondition(testPlayerData));
+        CaveObjective caveObjective = new CaveObjective(Color.BLUE, Color.RED, CaveOrientation.UPRIGHTL);
+        assertEquals(1, caveObjective.checkCondition(testPlayerData));
     }
 
+    @Test
+    void getTargetColor1() {
+    }
+
+    @Test
+    void getTargetColor2() {
+    }
+
+    @Test
+    void getOrientation() {
+    }
+
+    @Test
+    void checkCondition() {
+    }
 }
