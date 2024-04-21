bda80d0796a2ef33841caab4f6dd9f31356b72c3
Pietro_Ravasi <pietro2.ravasi@mail.polimi.it>
2024-04-21 16:45:01 +0200

    Test GM

--- src/test/java/it/polimi/sw/GC50/model/game/GamesManagerTest.java
+++ src/test/java/it/polimi/sw/GC50/model/game/GamesManagerTest.java
@@ -2,37 +2,85 @@ package it.polimi.sw.GC50.model.game;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 class GamesManagerTest {
 
+
+
     @Test
-    void getInstance() {
+    void testGetInstance() {
+        GamesManager m= GamesManager.getInstance();
+        assertNotEquals(null,m);
     }
 
     @Test
-    void addPlayer() {
+    void testAddPlayer() {
+        GamesManager m= GamesManager.getInstance();
+        Player pl2=new Player("pl2");
+        m.setGame("GAME1",2,20,new Player("pl1"));
+        m.addPlayer(pl2);
+       assertEquals(true,m.containsPlayer(pl2));
     }
 
     @Test
-    void removePlayer() {
+    void testRemovePlayer() {
+        GamesManager m= GamesManager.getInstance();
+        Player pl2=new Player("pl2");
+        m.setGame("GAME1",2,20,new Player("pl1"));
+        m.addPlayer(pl2);
+        assertEquals(true,m.containsPlayer(pl2));
+        m.removePlayer(pl2);
+        assertEquals(false,m.containsPlayer(pl2));
+
     }
 
     @Test
-    void containsPlayer() {
+    void testContainsPlayer() {
+        GamesManager m= GamesManager.getInstance();
+        Player pl2=new Player("pl2");
+        m.setGame("GAME1",2,20,new Player("pl1"));
+        m.addPlayer(pl2);
+        assertEquals(true,m.containsPlayer(pl2));
     }
 
     @Test
-    void setGame() {
+    void testSetGame() {
+        GamesManager m= GamesManager.getInstance();
+        Player pl2=new Player("pl2");
+        m.setGame("GAME1",2,20,new Player("pl1"));
+        assertEquals(true,m.containsGame("GAME1"));
     }
 
     @Test
-    void deleteGame() {
+    void testDeleteGame() {
+        GamesManager m= GamesManager.getInstance();
+        Player pl2=new Player("pl2");
+        m.setGame("GAME1",2,20,new Player("pl1"));
+        assertEquals(true,m.containsGame("GAME1"));
+        m.deleteGame("GAME1");
+        assertEquals(false,m.containsGame("GAME1"));
+
     }
 
     @Test
-    void containsGame() {
+    void testContainsGame() {
+        GamesManager m= GamesManager.getInstance();
+        Player pl2=new Player("pl2");
+        m.setGame("GAME1",2,20,new Player("pl1"));
+        assertEquals(true,m.containsGame("GAME1"));
+        assertEquals(false,m.containsGame("ciao"));
     }
 
     @Test
-    void getGame() {
+    void testGetGame() {
+        GamesManager m= GamesManager.getInstance();
+        Player pl2=new Player("pl2");
+        m.setGame("GAME1",2,20,new Player("pl1"));
+        Game g1= new Game("GAME1",2,20,new Player("pl1"));
+        assertEquals(g1,m.getGame("GAME1"));
+        assertEquals(null,m.getGame("GAME2"));
+
     }
 }
