52c1cdfc4939c6821252df416ff381bf4fca064d
annalisa pellicari <annalisa.pellicari@mail.polimi.it>
2024-04-29 15:51:31 +0200

    Aggiunti test su BlankBonusTest, CoveredCornersBonusTest, ResourceBonus

--- src/test/java/it/polimi/sw/GC50/model/card/BlankBonusTest.java
+++ src/test/java/it/polimi/sw/GC50/model/card/BlankBonusTest.java
@@ -1,10 +1,25 @@
 package it.polimi.sw.GC50.model.card;
 
+import it.polimi.sw.GC50.model.game.PlayerData;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BlankBonusTest {
 
     @Test
     void checkBonus() {
+        BlankBonus bonus = new BlankBonus();
+        PlayerData playerData = new PlayerData(40);
+        Corner[] corners = new Corner[]
+                {CornerTest.emptyCorner, CornerTest.emptyCorner, CornerTest.emptyCorner, CornerTest.emptyCorner};
+        List<Resource> fixedResources = new ArrayList<>(Arrays.asList(Resource.ANIMAL, Resource.PLANT));
+        PlayableCard card = new PlayableCard(Color.BLUE, 3, bonus, fixedResources, corners);
+        assertEquals(1,bonus.checkBonus(card,playerData,1,2));
+
     }
 }
--- src/test/java/it/polimi/sw/GC50/model/card/ResourcesBonusTest.java
+++ src/test/java/it/polimi/sw/GC50/model/card/ResourcesBonusTest.java
@@ -1,14 +1,27 @@
 package it.polimi.sw.GC50.model.card;
 
+import it.polimi.sw.GC50.model.game.PlayerData;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ResourcesBonusTest {
-
     @Test
-    void getTargetResource() {
+    void ResourceBonusTest(){
+        ResourcesBonus resourcesBonus = new ResourcesBonus(Resource.PLANT);
+        assertEquals(resourcesBonus.getTargetResource(),Resource.PLANT);
     }
-
     @Test
+
     void checkBonus() {
+
     }
+
+
+
+
 }
