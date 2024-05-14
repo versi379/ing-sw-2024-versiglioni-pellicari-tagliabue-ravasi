facbadc6a063c80716b1aee3a0d2820dc311a2b7
Giovanni <giovanniversiglioni@outlook.it>
2024-05-14 16:38:29 +0200

    temp (GUI) tbf

--- src/main/java/it/polimi/sw/GC50/view/GUI/GuiView.java
+++ src/main/java/it/polimi/sw/GC50/view/GUI/GuiView.java
@@ -5,19 +5,74 @@ import it.polimi.sw.GC50.model.game.DrawingPosition;
 import it.polimi.sw.GC50.net.gameMexNet.ModelMex;
 import it.polimi.sw.GC50.net.gameMexNet.PlaceCardMex;
 import it.polimi.sw.GC50.net.util.Request;
+import it.polimi.sw.GC50.view.GUI.controllers.GUIController;
+import it.polimi.sw.GC50.view.GUI.scenes.SceneInfo;
+import it.polimi.sw.GC50.view.GUI.scenes.ScenePath;
 import it.polimi.sw.GC50.view.View;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class GuiView implements View {
+import java.io.IOException;
+import java.util.ArrayList;
 
-    ModelMex modelmex;
+public class GuiView extends Application implements View {
 
-    private GUIApplication guiApplication;
+    private Stage primaryStage;
+    private StackPane root;
+    private ArrayList<SceneInfo> scenes;
+    private ModelMex modelmex;
 
-    public GuiView() {
-        new Thread(() -> {
-            Application.launch(GUIApplication.class);
-        }).start();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        loadScenes();
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Codex Naturalis");
+        root = new StackPane();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * This method uses the FXMLLoader to load the scenes of the game
+     * and their respective controllers, for each client.
+     */
+    private void loadScenes() {
+        FXMLLoader loader;
+        GUIController controller;
+        for (int i = 0; i < ScenePath.values().length; i++) {
+            loader = new FXMLLoader(getClass().getResource(ScenePath.values()[i].getPath()));
+            try {
+                root = loader.load();
+                controller = loader.getController();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            scenes.add(new SceneInfo(new Scene(root), ScenePath.values()[i], controller));
+        }
+    }
+
+    /**
+     * This method extracts the controller of a given scene.
+     */
+    public GUIController getController(ScenePath scenePath) {
+        int index = getSceneIndex(scenePath);
+        if (index != -1) {
+            return scenes.get(getSceneIndex(scenePath)).getController();
+        }
+        return null;
+    }
+
+    private int getSceneIndex(ScenePath scenePath) {
+        for (int i = 0; i < scenes.size(); i++) {
+            if (scenes.get(i).getScenePath().equals(scenePath))
+                return i;
+        }
+        return -1;
     }
 
     @Override
@@ -89,4 +144,5 @@ public class GuiView implements View {
     public DrawingPosition choseWhereToDraw() {
         return null;
     }
+
 }
