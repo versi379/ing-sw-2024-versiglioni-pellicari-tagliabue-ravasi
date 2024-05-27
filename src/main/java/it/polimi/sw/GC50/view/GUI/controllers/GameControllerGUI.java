8434c7416b1a1be88c6ee67995d71e0a9bbd000b
Giovanni <giovanniversiglioni@outlook.it>
2024-05-28 00:06:52 +0200

    added images (two codes to test -> working V)

--- src/main/java/it/polimi/sw/GC50/view/GUI/controllers/GameControllerGUI.java
+++ src/main/java/it/polimi/sw/GC50/view/GUI/controllers/GameControllerGUI.java
@@ -2,15 +2,22 @@ package it.polimi.sw.GC50.view.GUI.controllers;
 
 import it.polimi.sw.GC50.app.AppClient;
 import it.polimi.sw.GC50.view.GUI.GuiView;
+import it.polimi.sw.GC50.view.GUI.scenes.ScenePath;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * Controller for Game FXML scene.
  */
 public class GameControllerGUI {
+
+    @FXML
+    private AnchorPane pane;
 
     private GuiView guiView;
 
@@ -20,11 +27,23 @@ public class GameControllerGUI {
     @FXML
     private Label secretObjectivesLabel;
 
+    private ImageView imageView;
+
     @FXML
     public void initialize() {
         guiView = (GuiView) AppClient.getView();
         commonObjectivesLabel.setText(guiView.setupCommonObjectives);
         secretObjectivesLabel.setText(guiView.setupSecretObjectives);
+        Image starterCardFront = new Image(String.valueOf(getClass().getResource("/cards/fronts/30022.jpg")));
+        imageView = new ImageView(starterCardFront);
+        imageView.setFitWidth(200);
+        imageView.setFitHeight(150);
+        pane.getChildren().add(imageView);
+        Image starterCardBack = new Image(String.valueOf(getClass().getResource("/cards/backs/0002.jpg")));
+        imageView = new ImageView(starterCardBack);
+        imageView.setFitWidth(200);
+        imageView.setFitHeight(150);
+        pane.getChildren().add(imageView);
     }
 
 
