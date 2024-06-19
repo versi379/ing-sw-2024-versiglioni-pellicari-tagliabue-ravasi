3fd3d6278c56d9326a6e38a31467ac099c662c8b
annalisa pellicari <annalisa.pellicari@mail.polimi.it>
2024-06-19 21:42:53 +0200

    aggiunti commenti

--- src/main/java/it/polimi/sw/GC50/net/socket/NotifyMessage.java
+++ src/main/java/it/polimi/sw/GC50/net/socket/NotifyMessage.java
@@ -9,15 +9,26 @@ public class NotifyMessage implements Serializable {
     private final Notify notify;
     private final Message content;
 
+    /**
+     * Constructs an instance of NotifyMessage
+     * @param notify    notify of the message
+     * @param content   content of the message
+     */
     public NotifyMessage(Notify notify, Message content) {
         this.notify = notify;
         this.content = content;
     }
 
+    /**
+     * @return notify
+     */
     public Notify getNotify() {
         return notify;
     }
 
+    /**
+     * @return content of the message
+     */
     public Message getContent() {
         return content;
     }
--- src/main/java/it/polimi/sw/GC50/net/socket/RequestMessage.java
+++ src/main/java/it/polimi/sw/GC50/net/socket/RequestMessage.java
@@ -6,15 +6,26 @@ public class RequestMessage implements Serializable {
     private final Request request;
     private final Object content;
 
+    /**
+     * Constructs an instance of RequestMessage
+     * @param request    of the message
+     * @param content    of the message
+     */
     public RequestMessage(Request request, Object content) {
         this.request = request;
         this.content = content;
     }
 
+    /**
+     * @return message request
+     */
     public Request getRequest() {
         return request;
     }
 
+    /**
+     * @return content of the message
+     */
     public Object getContent() {
         return content;
     }
