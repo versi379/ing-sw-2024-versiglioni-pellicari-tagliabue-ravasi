9f554cbe3514de11e8c061fb3aaa2d6b84bd0e6c
Giovanni <giovanniversiglioni@outlook.it>
2024-06-21 11:22:07 +0200

    place card error fixing

--- src/main/java/it/polimi/sw/GC50/net/client/Client.java
+++ src/main/java/it/polimi/sw/GC50/net/client/Client.java
@@ -662,7 +662,7 @@ public class Client {
                 if (gameView.getNickname().equals(errorMex.getNickname())) {
                     view.showError(errorMex.getContent());
                 }
-                //((GuiView) view).serverError = true;
+                ((GuiView) view).playerAreaUpdated = true;
             }
 
             default -> {
